7fbf0ab95fdd6904f5d25892b5eee9d3274b6b5a
goliatone <140582+goliatone@users.noreply.github.com>
2024-12-03 19:44:24 -0800

    fix: default logger handle WithErrorHandler log

--- logger.go
+++ logger.go
@@ -21,6 +21,11 @@ func (d *defaultLogger) Info(format string, args ...any) {
 
 func (d *defaultLogger) Error(format string, args ...any) {
 	if LoggerEnabled {
-		fmt.Printf("[ERROR] "+format+"\n", args...)
+		switch t := args[0].(type) {
+		case map[string]any:
+			fmt.Printf("[ERROR] %s %+v\n", format, t)
+		default:
+			fmt.Printf("[ERROR] "+format+"\n", args...)
+		}
 	}
 }

9b860e048741593c13cd47ce0a7009e8aedf5a21
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-03 18:59:41 -0800

    refactor: combine meta structs

--- meta.go
+++ meta.go
@@ -2,53 +2,46 @@ package router
 
 func NewRouteDefinition() *RouteDefinition {
 	return &RouteDefinition{
-		metadata: &RouteMetadata{
-			Tags:       make([]string, 0),
-			Parameters: make([]Parameter, 0),
-			Responses:  make([]Response, 0),
-		},
+		Tags:       make([]string, 0),
+		Parameters: make([]Parameter, 0),
+		Responses:  make([]Response, 0),
 	}
-}
-
-// TODO: Either use lowercase or functions
-type RouteDefinition struct {
-	metadata *RouteMetadata
 }
 
 // Ensure RouteDefinition implements RouteInfo
 
 func (r *RouteDefinition) SetName(n string) RouteInfo {
-	r.metadata.Name = n
+	r.Name = n
 	return r
 }
 
 func (r *RouteDefinition) SetDescription(d string) RouteInfo {
-	r.metadata.Description = d
+	r.Description = d
 	return r
 }
 
 func (r *RouteDefinition) SetSummary(s string) RouteInfo {
-	r.metadata.Summary = s
+	r.Summary = s
 	return r
 }
 
 func (r *RouteDefinition) AddTags(t ...string) RouteInfo {
-	r.metadata.Tags = append(r.metadata.Tags, t...)
+	r.Tags = append(r.Tags, t...)
 	return r
 }
 
-func (r *RouteDefinition) AddParameter(name, in string, required bool, schema any) RouteInfo {
-	r.metadata.Parameters = append(r.metadata.Parameters, Parameter{
+func (r *RouteDefinition) AddParameter(name, in string, required bool, schema map[string]any) RouteInfo {
+	r.Parameters = append(r.Parameters, Parameter{
 		Name:     name,
 		In:       in,
 		Required: required,
-		Schema:   schema.(map[string]any),
+		Schema:   schema,
 	})
 	return r
 }
 
 func (r *RouteDefinition) SetRequestBody(desc string, required bool, content map[string]any) RouteInfo {
-	r.metadata.RequestBody = &RequestBody{
+	r.RequestBody = &RequestBody{
 		Description: desc,
 		Required:    required,
 		Content:     content,
@@ -57,7 +50,7 @@ func (r *RouteDefinition) SetRequestBody(desc string, required bool, content map
 }
 
 func (r *RouteDefinition) AddResponse(code int, desc string, content map[string]any) RouteInfo {
-	r.metadata.Responses = append(r.metadata.Responses, Response{
+	r.Responses = append(r.Responses, Response{
 		Code:        code,
 		Description: desc,
 		Content:     content,

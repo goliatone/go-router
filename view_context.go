46063ba03edd1d62c219dd23b043ed4f06930fbf
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-23 17:40:36 -0300

    add: view context to serialize variables

---
+++ view_context.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"encoding/json"
+
+	"github.com/flosch/pongo2/v6"
+)
+
+// // PageContext holds template data
+type PageContextx pongo2.Context
+
+// Update updates this context with the key/value-pairs from another context.
+func (c ViewContext) Update(other ViewContext) ViewContext {
+	for k, v := range other {
+		c[k] = v
+	}
+	return c
+}
+
+// SerializeAsContext will return any object as a PageContext instance
+func SerializeAsContext(m any) (map[string]any, error) {
+	// if ctx, ok := m.(map[string]any); ok {
+	// 	return ctx, nil
+	// }
+
+	var b []byte
+	var err error
+
+	if s, ok := m.(Serializer); ok {
+		b, err = s.Serialize()
+	} else {
+		b, err = json.Marshal(m)
+	}
+
+	if err != nil {
+		return map[string]any{}, err
+	}
+
+	ctx := map[string]any{}
+	err = json.Unmarshal(b, &ctx)
+	return ctx, err
+}

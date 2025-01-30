5a57f5dc185ce5cc304c45e3e42b2e3f85a90f52
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-30 19:09:25 -0300

    add: cookie imp

---
+++ cookie.go
@@ -0,0 +1,24 @@
+package router
+
+import "time"
+
+const (
+	CookieSameSiteDisabled   = "disabled" // not in RFC, just control "SameSite" attribute will not be set.
+	CookieSameSiteLaxMode    = "lax"
+	CookieSameSiteStrictMode = "strict"
+	CookieSameSiteNoneMode   = "none"
+)
+
+// Cookie data for c.Cookie
+type Cookie struct {
+	Name        string    `json:"name"`
+	Value       string    `json:"value"`
+	Path        string    `json:"path"`
+	Domain      string    `json:"domain"`
+	MaxAge      int       `json:"max_age"`
+	Expires     time.Time `json:"expires"`
+	Secure      bool      `json:"secure"`
+	HTTPOnly    bool      `json:"http_only"`
+	SameSite    string    `json:"same_site"`
+	SessionOnly bool      `json:"session_only"`
+}

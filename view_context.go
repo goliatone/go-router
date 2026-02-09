11f7e56a8388cb8e6505103221fbae7d639dd494
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-08 23:21:38 -0800

    fix: view context serialization

--- view_context.go
+++ view_context.go
@@ -1,7 +1,10 @@
 package router
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -33,7 +36,63 @@ func SerializeAsContext(m any) (map[string]any, error) {
 		return map[string]any{}, err
 	}
 
-	ctx := map[string]any{}
-	err = json.Unmarshal(b, &ctx)
-	return ctx, err
+	var ctx map[string]any
+	dec := json.NewDecoder(bytes.NewReader(b))
+	dec.UseNumber()
+
+	if err := dec.Decode(&ctx); err != nil {
+		return map[string]any{}, err
+	}
+
+	if err := ensureJSONEOF(dec); err != nil {
+		return map[string]any{}, err
+	}
+
+	if ctx == nil {
+		return map[string]any{}, nil
+	}
+
+	return normalizeJSONNumbers(ctx).(map[string]any), nil
+}
+
+func ensureJSONEOF(dec *json.Decoder) error {
+	var trailing any
+	err := dec.Decode(&trailing)
+	if err == io.EOF {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return io.ErrUnexpectedEOF
+}
+
+func normalizeJSONNumbers(v any) any {
+	switch val := v.(type) {
+	case map[string]any:
+		for k, item := range val {
+			val[k] = normalizeJSONNumbers(item)
+		}
+		return val
+	case []any:
+		for i, item := range val {
+			val[i] = normalizeJSONNumbers(item)
+		}
+		return val
+	case json.Number:
+		raw := val.String()
+		if !strings.ContainsAny(raw, ".eE") {
+			if n, err := val.Int64(); err == nil {
+				return n
+			}
+		}
+
+		if n, err := val.Float64(); err == nil {
+			return n
+		}
+
+		return raw
+	default:
+		return v
+	}
 }

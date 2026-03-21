ebb02797f9c208029fd35d9c37b4e815869209a0
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-20 18:38:59 -0700

    Bump version: v0.54.0

--- websocket_client_handlers.go
+++ websocket_client_handlers.go
@@ -30,7 +30,7 @@ var websocketTestHTML []byte
 
 // Version and build information
 const (
-	WebSocketClientVersion = "0.53.0"
+	WebSocketClientVersion = "0.54.0"
 	WebSocketClientBuild   = "production"
 )
 

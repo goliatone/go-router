599354f48de15aa80555ac840e16f4a736b333b3
goliatone <140582+goliatone@users.noreply.github.com>
2026-05-28 18:28:38 -0700

    add: fiber keepalive implementation

---
+++ websocket_keepalive.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"sync"
+	"time"
+)
+
+func startWebSocketPingLoop(wsCtx WebSocketContext, config WebSocketConfig, logger Logger) func() {
+	if wsCtx == nil || !config.keepAliveEnabled() {
+		return func() {}
+	}
+
+	ticker := time.NewTicker(config.PingPeriod)
+	done := make(chan struct{})
+	var stopOnce sync.Once
+
+	go func() {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				if err := wsCtx.WritePing(nil); err != nil {
+					if logger != nil {
+						logger.Info("WebSocket ping error", "error", err)
+					}
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return func() {
+		stopOnce.Do(func() {
+			close(done)
+		})
+	}
+}
